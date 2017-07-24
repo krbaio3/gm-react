28343429f10949a7b1671749119725e601bb8ddd
KrBaIO3 <krbaio3@gmail.com>
2017-07-24 20:15:31 +0200

    Config init
    chore(configuracion incial part1)

---
+++ src/client/main.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { HelloComponent } from './app/hello';
+
+ReactDOM.render(
+  <HelloComponent/>,
+  document.getElementById('root'),
+);
