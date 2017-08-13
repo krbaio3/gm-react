f342a1e16d53c9ca18712250eb73953c28b7e6bc
KrBaIO3 <krbaio3@gmail.com>
2017-08-13 23:17:46 +0200

    chore(change port to develop mode)

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         contentBase: './dist', // Content base
         inline: true, // Enable watch and live reload
         host: 'localhost',
-        port: 8080,
+        port: 8088,
         stats: 'errors-only'
     },
 
