40b81706e2edce55ce3ead0cf9ebf4f563c7ca07
Jorge Carballo Alonso <A181226@cm.es>
2017-08-17 12:57:50 +0200

    chore(package.json => add reactstrap and dependencies) feat (home.tsx => add buttons, core folder => new structure)

--- src/client/app/home.component/home.tsx
+++ src/client/app/home.component/home.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
-import { VehicleComponent } from '../vehicle.component/vehicle';
+// import { VehicleComponent } from '../vehicle.component/vehicle';
+// import { NavBar } from '../core/navBar/navBar';
+require('./home.scss');
 
 export const HomeComponent = () => {
   return (
-    <VehicleComponent></VehicleComponent>
+    <div>
+      {/*<NavBar></NavBar>*/}
+      <div className="container" id="home">
+        <div className="row">
+          <button className="salto col-12">
+            <i className="fa fa-file-text-o fa-5x" aria-hidden="true" />
+            <span>orden de reparacion</span>
+          </button>
+          <button className="salto col-12">
+            <i className="fa fa-car fa-5x" aria-hidden="true" />
+            <span className="">vehiculos</span>
+          </button>
+          <button className="salto col-12">
+            <i className="fa fa-users fa-5x" aria-hidden="true" />
+            <span>clientes</span>
+          </button>
+          <button className="salto col-12">
+            <i className="fa fa-credit-card fa-5x" aria-hidden="true" />
+            <span>facturas</span>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
